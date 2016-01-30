b9f17f04ce41a0cf7753f87a626b1757588647a2
Rodger Rüdiger <post@rodger-ruediger.de>
2016-01-30 15:49:56 +0100

    [TASK] combines rvm and ruby install commands

--- build_unix.sh
+++ build_unix.sh
@@ -23,8 +23,7 @@ function _check_ruby() {
         case $yn in
             [Yy]* )
               gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-              \curl -sSL https://get.rvm.io | bash
-              rvm install ruby --latest
+              \curl -sSL https://get.rvm.io | bash -s stable --ruby
               if ! which ruby
                 then
                   printf "\033[$orange ##ruby not found although it should have been. Please try running this script again with a login shell. \033[0m \n";
